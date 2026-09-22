e2560f73b44df2e55cee4e444b1063160a9dcce8
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-09-23 01:35:16 +1000

    Add automatic memory reuse layer

---
+++ src/NV3047_Memory.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "NV3047_MemoryManager.h"
+#include "NV3047_ManagedBuffer.h"
+#include "NV3047_FramebufferPair.h"
+#include "NV3047_DMAPool.h"
+#include "NV3047_ObjectPool.h"
+#include "NV3047_AssetCache.h"
+#include "NV3047_AutoMemory.h"
